e6b7b625ee7c66f68def834f9c5e550cbac19761
dylanmydude <dylan@euca.co.za>
2024-09-10 12:54:47 +0200

    chat colours and /help commands

--- client.rb
+++ client.rb
@@ -1,16 +1,29 @@
 require 'socket'
+require 'time'
+
+# Method to display the list of commands
+def show_commands
+  puts "\033[36m"  # Light cyan colour
+  puts "\n=========* Commands *============="
+  puts "= Type 'exit' to leave the chatroom ="
+  puts "= /users to list all users         ="
+  puts "= /nick new_name to change nickname ="
+  puts "= /help to list all commands        ="
+  puts "===================================\033[0m"  # Reset to default colour
+end
 
 puts "Enter the server's IP address: "
 server_ip = gets.chomp
 client = TCPSocket.new(server_ip, 12345)
 
 puts "Connected to the chatroom. Type 'exit' to leave."
+show_commands
 
 Thread.new do
   loop do
     begin
       message = client.gets.chomp
-      puts message
+      puts message # Display incoming messages as they are formatted by the server
     rescue StandardError
       puts "Disconnected from the server."
       client.close
@@ -20,7 +33,6 @@ Thread.new do
 end
 
 loop do
-  print "You: "
   input = gets.chomp
   client.puts input
   
--- server.rb
+++ server.rb
@@ -1,31 +1,84 @@
 require 'socket'
+require 'time'
 
-server = TCPServer.new('0.0.0.0', 12345) # Bind
+# ANSI colour codes for different users
+USER_COLOURS = [
+  "\033[31m", # Red
+  "\033[32m", # Green
+  "\033[33m", # Yellow
+  "\033[34m", # Blue
+  "\033[35m", # Magenta
+  "\033[36m", # Cyan
+  "\033[91m", # Light Red
+  "\033[92m", # Light Green
+  "\033[93m", # Light Yellow
+  "\033[94m", # Light Blue
+  "\033[95m", # Light Magenta
+  "\033[96m"  # Light Cyan
+]
+
+RESET_COLOR = "\033[0m"  # Reset to default colour
+
+server = TCPServer.new('0.0.0.0', 12345) # Bind to all network interfaces
 clients = []
+usernames = {} # To keep track of client usernames
+user_colours = {} # To keep track of each user's assigned colour
 
 puts "Chatroom server started. Waiting for clients to connect..."
 
+# Method to broadcast messages to all clients
+def broadcast(clients, sender, message, sender_color)
+  timestamp = Time.now.strftime("%H:%M:%S")
+  formatted_message = "#{sender_color}#{sender} (#{timestamp}): #{message}#{RESET_COLOR}"
+  clients.each do |c|
+    c.puts formatted_message unless c == sender
+  end
+end
+
 loop do
- 
   client = server.accept
   clients << client
-  puts "New client connected: #{client.peeraddr[2]}"
+  user_colours[client] = USER_COLOURS[clients.size % USER_COLOURS.size] # Assign a colour
+  usernames[client] = client.peeraddr[2] # Default username is the IP address
+  puts "New client connected: #{usernames[client]}"
 
   Thread.new(client) do |conn|
-    conn.puts "Welcome to the chatroom!"
-    
+    conn.puts "Welcome to the chatroom! Type /help for a list of commands."
+
     loop do
       begin
         message = conn.gets.chomp
-        puts "Received: #{message} from #{conn.peeraddr[2]}"
-        
-        clients.each do |c|
-          c.puts "#{conn.peeraddr[2]} says: #{message}" unless c == conn
+        puts "Received: #{message} from #{usernames[conn]}"
+
+        case message
+        when '/help'
+          conn.puts "\033[36m\n=========* Commands *=============\n" +
+                    "= Type 'exit' to leave the chatroom =\n" +
+                    "= /users to list all users         =\n" +
+                    "= /nick new_name to change nickname =\n" +
+                    "= /help to list all commands        =\n" +
+                    "===================================\033[0m"
+        when '/users'
+          conn.puts "Connected users: #{usernames.values.join(', ')}"
+        when /^\/nick\s+(.+)/
+          new_name = message.match(/^\/nick\s+(.+)/)[1]
+          old_name = usernames[conn]
+          usernames[conn] = new_name
+          broadcast(clients, "#{old_name} (#{conn.peeraddr[2]})", "has changed their name to #{new_name}.", user_colours[conn])
+        when 'exit'
+          puts "Client disconnected: #{usernames[conn]}"
+          clients.delete(conn)
+          broadcast(clients, usernames[conn], "has left the chatroom.", user_colours[conn])
+          conn.close
+          break
+        else
+          # Broadcast the message with the assigned user colour
+          broadcast(clients, usernames[conn], message, user_colours[conn])
         end
-        
       rescue StandardError
-        puts "Client disconnected: #{conn.peeraddr[2]}"
+        puts "Client disconnected: #{usernames[conn]}"
         clients.delete(conn)
+        broadcast(clients, usernames[conn], "has left the chatroom.", user_colours[conn])
         conn.close
         break
       end
